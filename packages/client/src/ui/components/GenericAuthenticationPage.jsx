4fda73443477e3fdcdfd3cc539a9c2db769c4bbd
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-08-18 13:36:37 -0400

    make login/register/guest page look nicer

--- packages/client/src/ui/components/GenericAuthenticationPage.jsx
+++ packages/client/src/ui/components/GenericAuthenticationPage.jsx
@@ -3,13 +3,19 @@ import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles({
   bigSmileyFace: {
+    // make it large
     width: "512px",
     height: "512px",
-    imageRendering: "crisp-edges"
+    // when upscaling, don't make it blurry
+    imageRendering: "crisp-edges",
+    // center it
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto"
   }
 })
 
@@ -25,19 +31,20 @@ export default ({ smileyUrl, inputs, submit }) => {
   }
 
   return (
-    <>
+    <Container component="main" maxWidth="sm">
       <img className={styles.bigSmileyFace} src={smileyUrl} />
       {inputs.map((input, index) => (
-        <React.Fragment key={index}>
-          <Typography>
-            {typeof input.text === "function" ? input.text(values[index]) : input.text}
-          </Typography>
-          <TextField onChange={({ target: { value } }) => setValues[index](value)} />
-        </React.Fragment>
+        <TextField
+          fullWidth
+          key={index}
+          label={typeof input.text === "function" ? input.text(values[index]) : input.text}
+          onKeyDown={({ keyCode }) => keyCode === 13 ? submit(values) : undefined}
+          onChange={({ target: { value } }) => setValues[index](value)}
+        />
       ))}
-      <Button onClick={() => submit(values)}>
+      <Button fullWidth onClick={() => submit(values)}>
         Go!
       </Button>
-    </>
+    </Container>
   );
 };
