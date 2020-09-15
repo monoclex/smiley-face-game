961f0eb02eb12f3412d1006950e8b94359f87815
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-09-15 09:34:27 -0400

    Update Username.ts

--- packages/api/src/schemas/Username.ts
+++ packages/api/src/schemas/Username.ts
@@ -1,7 +1,8 @@
 import Schema, { string, Type } from "computed-types";
 
-const usernameRegex = /[A-Za-z0-9_]{3,20}/;
+const usernameRegex = /[A-Za-z0-9_]/;
 
-export const UsernameSchema = Schema(string.regexp(usernameRegex));
+// .min(3).max(20) is being used because despite adding {3,20} to the regex, it seemed to not limit the username length
+export const UsernameSchema = Schema(string.regexp(usernameRegex).min(3).max(20));
 export type Username = Type<typeof UsernameSchema>;
 export const validateUsername = UsernameSchema.destruct();
