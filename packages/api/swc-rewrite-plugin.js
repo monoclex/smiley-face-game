ba6906549b72c2b008393e62c912cd8132b42499
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-09-24 18:18:49 -0400

    use swc to build api & build in parallel

---
+++ packages/api/swc-rewrite-plugin.js
@@ -0,0 +1,23 @@
+const Visitor = require("@swc/core/Visitor").default;
+const path = require("path");
+
+/** @param {import("@swc/core").Program} program */
+module.exports = (program) => {
+  for (const item of program.body) {
+    if (item.type !== "ImportDeclaration") continue;
+
+    const declaration = item;
+
+    if (declaration.source.value.startsWith("@smiley-face-game/api")) {
+      declaration.source.value = declaration.source.value.replace("@smiley-face-game/api", path.resolve(__dirname, "./src"));
+      continue;
+    }
+    
+    if (declaration.source.value.startsWith("@/")) {
+      declaration.source.value = declaration.source.value.replace("@/", path.resolve(__dirname, "./src/") + "/")
+      continue;
+    }
+  }
+
+  return program;
+};
