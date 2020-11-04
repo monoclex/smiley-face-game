3571a2ce6f20404056baa04bf9345787190f81b5
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-11-04 11:12:56 -0500

    use brown tile

--- packages/client/src/game/tiles/FullTile.ts
+++ packages/client/src/game/tiles/FullTile.ts
@@ -13,26 +13,14 @@ export default class FullTile implements Tile<TileId.Basic> {
   place(tile: Phaser.Tilemaps.Tile, tileState: TileState & { id: TileId.Basic }): void {
     tile.setCollision(true);
     tile.index = this.index(tileState.color);
-
-    if (tileState.color === "brown") {
-      tile.tint = 0x70_42_14;
-    }
   }
 
   onRemove(tile: Phaser.Tilemaps.Tile) {
-    tile.tint = 0xff_ff_ff;
   }
 
   renderCanvas({ getFrame, context, block }: RenderCanvasParams<TileId.Basic>) {
     const { x, y, width, height, atlas } = getFrame(this.index(block.color));
     context.drawImage(atlas, x, y, width, height, 0, 0, 32, 32);
-
-    // TODO: get brown block color
-    if (block.color === "brown") {
-      context.fillStyle = "#704214";
-      context.globalAlpha = 0.9;
-      context.fillRect(0, 0, 32, 32);
-    }
   }
 
   private index(color: Color | null | undefined): number {
@@ -45,7 +33,7 @@ export default class FullTile implements Tile<TileId.Basic> {
         return mapTileNameToClientId("basic-black");
 
       case "brown":
-        return mapTileNameToClientId("basic-white");
+        return mapTileNameToClientId("basic-brown");
 
       case "red":
         return mapTileNameToClientId("basic-red");
