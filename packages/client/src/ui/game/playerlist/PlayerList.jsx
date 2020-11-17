829dceccb2a7372d9d31c35b4263b0156ff44ea9
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-11-17 01:04:42 -0500

    only display menu in playerlist when you can perform actions

--- packages/client/src/ui/game/playerlist/PlayerList.jsx
+++ packages/client/src/ui/game/playerlist/PlayerList.jsx
@@ -80,7 +80,33 @@ const Player = ({ username, playerId, role: roleParam }) => {
 
   const mainPlayer = useRecoilValue(currentPlayer) ?? { username: "", role: "non", playerId: -1 };
 
+  /** @type {JSX.Element[]} */
+  const actions = [];
+
+  // make sure that when you add things to `actions` you can guarantee a static order
+  // so that the `key` prop can be set to the index it's at in the array
+  if (mainPlayer.role === "owner") {
+    actions.push(
+      <ToggleButton
+        value="edit"
+        aria-label="edit"
+        selected={role === "edit"}
+        onChange={() => setEdit(!(role === "edit"))}
+      >
+        <Pencil />
+      </ToggleButton>
+    );
+
+    actions.push(
+      <MenuItem onClick={kick}>
+        <ShoeCleat />
+      </MenuItem>
+    );
+  }
+
   const handleClick = (event) => {
+    // don't show menu if there are no actions to perform
+    if (actions.length === 0) return;
     setAnchorElement(event.currentTarget);
   };
 
@@ -127,21 +153,8 @@ const Player = ({ username, playerId, role: roleParam }) => {
         onClose={handleClose}
       >
         <Grid container justify="center" direction="column">
-          {mainPlayer.role === "owner" && (
-            <>
-              <ToggleButton
-                value="edit"
-                aria-label="edit"
-                selected={role === "edit"}
-                onChange={() => setEdit(!(role === "edit"))}
-              >
-                <Pencil />
-              </ToggleButton>
-              <MenuItem onClick={kick}>
-                <ShoeCleat />
-              </MenuItem>
-            </>
-          )}
+          {/* it's fine to have the key be the index because we can guarantee that we add `actions` in a static order */}
+          <>{actions.map((action, i) => ({ ...action, key: i }))}</>
         </Grid>
       </Menu>
     </>
