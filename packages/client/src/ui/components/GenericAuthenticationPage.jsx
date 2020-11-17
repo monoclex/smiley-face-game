f9c2a5d325bc8ab45cca3eabb16a192e0ef96eae
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-11-17 01:46:06 -0500

    more responsive UI prompts

--- packages/client/src/ui/components/GenericAuthenticationPage.jsx
+++ packages/client/src/ui/components/GenericAuthenticationPage.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useSnackbar } from "notistack";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Container from "@material-ui/core/Container";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Grid from "@material-ui/core/Grid";
 import { zUsername, zPassword, zEmail } from "@smiley-face-game/common/types";
 
 const useStyles = makeStyles({
@@ -46,6 +49,21 @@ const validators = {
 export default ({ smileyUrl, inputs, submit }) => {
   const styles = useStyles();
   const { handleSubmit, register, errors, watch } = useForm();
+  const { enqueueSnackbar } = useSnackbar();
+  const [isWorking, setWorking] = useState(false);
+
+  const onSubmit = (payload) => {
+    setWorking(true);
+    submit(payload)
+      .catch((error) => {
+        enqueueSnackbar(error.toString(), {
+          variant: "error",
+        });
+      })
+      .finally(() => {
+        setWorking(false);
+      });
+  };
 
   return (
     <Container component="main" maxWidth="sm">
@@ -58,24 +76,33 @@ export default ({ smileyUrl, inputs, submit }) => {
         )}
         src={smileyUrl}
       />
-      <form onSubmit={handleSubmit(submit)}>
-        {inputs.map((input, index) => (
-          <TextField
-            key={index}
-            type={input?.type}
-            fullWidth
-            id={input.name}
-            name={input.name}
-            label={typeof input.text === "function" ? input.text(watch(input.name)) : input.text}
-            error={!!(errors && errors[input.name])}
-            helperText={errors && errors[input.name]?.message}
-            inputRef={register({ required: true, validate: validators[input.name] })}
-          />
-        ))}
-        <Button fullWidth type="submit">
-          Go!
-        </Button>
-      </form>
+      {isWorking && (
+        <Grid container direction="row" justify="center" alignItems="center">
+          <Grid item>
+            <CircularProgress />
+          </Grid>
+        </Grid>
+      )}
+      {!isWorking && (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {inputs.map((input, index) => (
+            <TextField
+              key={index}
+              type={input?.type}
+              fullWidth
+              id={input.name}
+              name={input.name}
+              label={typeof input.text === "function" ? input.text(watch(input.name)) : input.text}
+              error={!!(errors && errors[input.name])}
+              helperText={errors && errors[input.name]?.message}
+              inputRef={register({ required: true, validate: validators[input.name] })}
+            />
+          ))}
+          <Button fullWidth type="submit">
+            Go!
+          </Button>
+        </form>
+      )}
     </Container>
   );
 };
