1fb441dcee86ec76c813674ee74432315ebaf320
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-11-26 04:31:17 -0500

    fix bad import & add alias so ts defs work

--- packages/client/src/ui/pages/PlayPage.jsx
+++ packages/client/src/ui/pages/PlayPage.jsx
@@ -11,7 +11,7 @@ import { Grid } from "@material-ui/core";
 import { globalVariableParkour, LoadingScene } from "../../scenes/loading/LoadingScene";
 import Chat from "../../ui/game/chat/Chat";
 import BlockBar from "../../ui/game/blockbar/BlockBar";
-import history from "ui/history";
+import history from "../history";
 import { isDev } from "../../isProduction";
 import GameScene from "../../game/GameScene";
 import { loadingState, loading as sharedGlobalLoading } from "../../recoil/atoms/loading";
