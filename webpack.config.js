525bd87fd6ed3ea8bef6deada308df1f903cd152
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-11-29 21:21:25 -0500

    Webpack 5 (#90)
    * use terser for minification
    
    * make the switch to webpack v5 (bundle size smaller!)
    
    * remove some stuff that didn't matter & fix env stuff
    
    * tell babel to ignore node_modules, improving bundlesize AND perf !!!

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,11 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 const path = require("path");
 const { DefinePlugin } = require("webpack");
 const atlas = require("rust-atlas-generator-webpack-plugin");
 
 module.exports = (env, argv) => {
-  const mode = argv.mode;
-  const bundle = argv.bundle;
+  const mode = env.mode;
 
   let plugins = [
     atlas({
@@ -18,7 +15,7 @@ module.exports = (env, argv) => {
       height: 32,
     }),
     new DefinePlugin({
-      "process.env.DEV": JSON.stringify(!!argv.dev),
+      "process.env.DEV": JSON.stringify(!!env.dev),
     }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
@@ -26,8 +23,6 @@ module.exports = (env, argv) => {
       hash: true,
     }),
   ];
-
-  // if (bundle) plugins.push(new BundleAnalyzerPlugin());
 
   return {
     mode,
@@ -38,14 +33,17 @@ module.exports = (env, argv) => {
     },
     module: {
       rules: [
-        { test: /\.(j|t)sx?$/, use: { loader: "babel-loader", options: require("./babel.config.js") } },
+        {
+          test: /\.(j|t)sx?$/,
+          include: path.resolve(__dirname, "./packages"),
+          use: { loader: "babel-loader", options: require("./babel.config.js") },
+        },
         { test: /\.(png|mp3)$/, use: "file-loader" },
         { test: /\.svg$/, use: "@svgr/webpack" },
       ],
     },
     plugins,
     output: {
-      filename: "[name].[hash].js",
       path: path.resolve(__dirname, "packages/client/dist"),
     },
     devServer: {
@@ -54,11 +52,7 @@ module.exports = (env, argv) => {
       hot: true,
     },
     optimization: {
-      splitChunks: {
-        chunks: "all",
-      },
-      minimize: false, // mode === "production",
-      minimizer: undefined, // mode === "production" ? [new TerserPlugin()] : undefined,
+      minimize: mode === "production",
     },
   };
 };
