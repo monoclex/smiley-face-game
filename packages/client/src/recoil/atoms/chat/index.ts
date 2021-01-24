f0847609e077ff76ee185b60d4d150fa91eefd74
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2021-01-24 09:23:04 +0000

    fix ipnut/chat/keyboard/cursor things

--- packages/client/src/recoil/atoms/chat/index.ts
+++ packages/client/src/recoil/atoms/chat/index.ts
@@ -1,4 +1,4 @@
-import { atom, selector } from "recoil";
+import { atom, DefaultValue, selector } from "recoil";
 import SharedGlobal from "../../SharedGlobal";
 import { gameState } from "../gameState";
 
@@ -9,13 +9,21 @@ export const messagesState = selector({
 
 interface ChatState {
   isActive: boolean;
+  settingsOpen: boolean;
 }
 
-export const defaultChatState: ChatState = { isActive: false };
+export const defaultChatState: ChatState = { isActive: false, settingsOpen: false };
 export const chat = new SharedGlobal<ChatState>(defaultChatState);
 export const chatState = atom<ChatState>({
   key: "chatState",
   default: defaultChatState,
   //@ts-ignore
   effects_UNSTABLE: [chat.initialize],
+});
+
+export const settingsOpen = selector<boolean>({
+  key: "settingsOpen",
+  get: ({ get }) => get(chatState).settingsOpen,
+  set: ({ set }, value) =>
+    set(chatState, (old) => ({ ...old, settingsOpen: value instanceof DefaultValue ? false : value })),
 });
