34faeb4e263070f2337759c2c2de0bbeb6f6086f
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2022-02-13 20:59:53 -0500

    reduce duplication

--- packages/client/src/bridge/Keyboard.ts
+++ packages/client/src/bridge/Keyboard.ts
@@ -1,6 +1,13 @@
 import { Connection } from "@smiley-face-game/api";
+import { Inputs } from "@smiley-face-game/api/game/Inputs";
 import { Player } from "@smiley-face-game/api/physics/Player";
 import inputEnabled from "./inputEnabled";
+
+function toggle<M, K extends keyof M, V extends M[K]>(inputs: M, key: K, pressed: V): boolean {
+  const didChange = inputs[key] !== pressed;
+  inputs[key] = pressed;
+  return didChange;
+}
 
 export default class Keyboard {
   constructor(readonly player: Player, readonly connection: Connection) {
@@ -32,6 +39,10 @@ export default class Keyboard {
     this.handleKey(key.toLowerCase(), down);
   }
 
+  toggle(key: keyof Inputs, pressed: boolean) {
+    return toggle(this.player.input, key, pressed);
+  }
+
   handleKey(key: string, pressed: boolean) {
     if (!inputEnabled()) return;
 
@@ -40,27 +51,22 @@ export default class Keyboard {
     switch (key) {
       case "arrowup":
       case "w":
-        didChange = this.player.input.up !== pressed;
-        this.player.input.up = pressed;
+        didChange = this.toggle("up", pressed);
         break;
       case "arrowright":
       case "d":
-        didChange = this.player.input.right !== pressed;
-        this.player.input.right = pressed;
+        didChange = this.toggle("right", pressed);
         break;
       case "arrowleft":
       case "a":
-        didChange = this.player.input.left !== pressed;
-        this.player.input.left = pressed;
+        didChange = this.toggle("left", pressed);
         break;
       case "arrowdown":
       case "s":
-        didChange = this.player.input.down !== pressed;
-        this.player.input.down = pressed;
+        didChange = this.toggle("down", pressed);
         break;
       case " ": // space
-        didChange = this.player.input.jump !== pressed;
-        this.player.input.jump = pressed;
+        didChange = this.toggle("jump", pressed);
         break;
       case "f":
       case "g":
