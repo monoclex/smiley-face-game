b0c62a23fb814bd2e01cbc6abd7dba8d640a18e4
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2022-02-17 16:29:15 -0500

    add tests for arrows

--- packages/api/__tests__/physics.ts
+++ packages/api/__tests__/physics.ts
@@ -3,10 +3,8 @@ import { Simulator } from "./helpers.test-helper";
 it("players can climb onto ledges with dots", () => {
   const simulation = new Simulator(
     `
-......
-.   X.
-.p d..
-......
+.   X
+.p d.
 `,
     {
       d: "dot",
@@ -19,4 +17,43 @@ it("players can climb onto ledges with dots", () => {
   simulation.simulateMs(1000);
 
   expect(simulation.player.worldPosition).toEqual(simulation.goal);
+});
+
+describe("up arrows are not strong", () => {
+  it("single arrow is not strong", () => {
+    const simulation = new Simulator(
+      `
+.  .
+.  .
+.p^.
+..X.
+....
+`
+    );
+
+    simulation.player.input.right = true;
+
+    simulation.simulateMs(560);
+
+    expect(simulation.player.worldPosition).toEqual(simulation.goal);
+  });
+
+  it("double arrow is not strong", () => {
+    const simulation = new Simulator(
+      `
+. .
+. .
+. .
+.p.
+.^.
+.^.
+.X.
+...
+`
+    );
+
+    simulation.simulateMs(2000);
+
+    expect(simulation.player.worldPosition).toEqual(simulation.goal);
+  });
 });
