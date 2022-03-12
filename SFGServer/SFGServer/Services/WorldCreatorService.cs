63856199b6e08b539ddb9c61e5dc2790b268946c
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2022-03-12 17:47:52 -0500

    generate world properly on account register

--- SFGServer/SFGServer/Services/WorldCreatorService.cs
+++ SFGServer/SFGServer/Services/WorldCreatorService.cs
@@ -1,5 +1,6 @@
 ﻿using SFGServer.DAL;
 using SFGServer.DAL.Models;
+using SFGServer.Game.Services;
 
 namespace SFGServer.Services
 {
@@ -7,28 +8,32 @@ namespace SFGServer.Services
     {
         private readonly SfgContext _sfgContext;
         private readonly GenerateWorldIdService _generateWorldIdService;
+        private readonly GenerateBlankWorldService _generateBlankWorldService;
 
-        public WorldCreatorService(SfgContext sfgContext, GenerateWorldIdService generateWorldIdService)
+        public WorldCreatorService(SfgContext sfgContext, GenerateWorldIdService generateWorldIdService, GenerateBlankWorldService generateBlankWorldService)
         {
             _sfgContext = sfgContext;
             _generateWorldIdService = generateWorldIdService;
+            _generateBlankWorldService = generateBlankWorldService;
         }
 
-        public World CreateDefaultWorld(Account account) => CreateWorld(account, 50, 50);
+        public Task<World> CreateDefaultWorld(Account account, CancellationToken cancellationToken = default) => CreateWorld(account, 50, 50, cancellationToken);
 
-        public World CreateWorld(Account owner, int width, int height)
+        public async Task<World> CreateWorld(Account owner, uint width, uint height, CancellationToken cancellationToken)
         {
+            if (width > int.MaxValue || height > int.MaxValue) throw new ArgumentException("width/height >= int max lol");
+
             var world = new World {
                 Id = _generateWorldIdService.GenerateSavedWorld().Id,
                 Owner = owner,
                 Name = $"{owner.Username}'s World",
-                Width = width,
-                Height = height,
-
-                // TODO(javascript): generate world data
-                RawWorldData = "[]",
-                WorldDataVersion = 2,
+                Width = (int)width,
+                Height = (int)height,
             };
+
+            var worldData = await _generateBlankWorldService.GenerateWorld(width, height, cancellationToken);
+            world.WorldDataVersion = worldData.worldDataVersion;
+            world.RawWorldData = worldData.worldData;
 
             _sfgContext.Worlds.Add(world);
 
