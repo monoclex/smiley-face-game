172607b2ef27fce56637de62ca569e883d9e45bd
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2022-03-18 12:48:50 -0400

    reduce bundle size by carefully importing sideeffectful module

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -4,32 +4,40 @@ import App from "./ui/App";
 import { useDev } from "@smiley-face-game/api";
 import { rewriteHost, useTest } from "@smiley-face-game/api/net/endpoints";
 import { routesRewritten } from "./rewritten";
-import whyDidYouRender from "@welldone-software/why-did-you-render";
 
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-    titleColor: "red",
-    trackHooks: true,
-    logOwnerReasons: true,
-    logOnDifferentValues: true,
-  });
 if (import.meta.env.SERVER_MODE === "localhost") {
   rewriteHost((endpoint) => ({ ...endpoint, host: window.location.hostname + ":5265/v1" }));
+
+  import("@welldone-software/why-did-you-render").then(({ default: whyDidYouRender }) => {
+    whyDidYouRender(React, {
+      trackAllPureComponents: true,
+      titleColor: "red",
+      trackHooks: true,
+      logOwnerReasons: true,
+      logOnDifferentValues: true,
+    });
+
+    render();
+  });
 } else if (import.meta.env.SERVER_MODE === "development") {
   // useDev();
   useTest();
+  render();
 } else if (import.meta.env.SERVER_MODE === "production") {
   // production servers are the default
+  render();
 }
 
 routesRewritten.resolve(undefined);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
 
 // enable HMR (hot module reloading)
 if (import.meta.webpackHot) {
